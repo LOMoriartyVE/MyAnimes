7f3b622cf8e849ac9cbc09a31477dc4fcda30978
Rky Memory <muh2003erf@gmail.com>
2026-07-14 10:39:34 +0300

    Ignore flutter_cache and stop tracking it

--- flutter_cache/hosted/pub.dev/get_it-9.2.1/example/android/app/src/main/kotlin/com/example/get_it_example/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.get_it_example
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- flutter_cache/hosted/pub.dev/msix-3.16.13/bin/publish.dart
+++
@@ -1,5 +0,0 @@
-import 'package:msix/msix.dart';
-
-Future<void> main(List<String> arguments) async {
-  await Msix(arguments).publish();
-}
